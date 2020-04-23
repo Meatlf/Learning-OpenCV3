225ce8cf2f1e3d2552aca4d10db82915bd55a8b6
Meatlf <454779758@qq.com>
2020-04-23 19:48:32 +0800

    Added Learning OpenCV3独立成新项目.

---
+++ code/code-my/Chapter06-绘图和注释/bin/CMakeFiles/opencv_test2.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ttz/Documents/StudyNote/05-OpenCV-学习笔记/code/Chapter06-绘图和注释/src/draw_circle.cc" "/home/ttz/Documents/StudyNote/05-OpenCV-学习笔记/code/Chapter06-绘图和注释/bin/CMakeFiles/opencv_test2.dir/src/draw_circle.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/opencv/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/code-my/Chapter06-绘图和注释/bin/CMakeFiles/opencv_test2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_test2.dir/src/draw_circle.cc.o"
+  "/bin/opencv_test2.pdb"
+  "/bin/opencv_test2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
